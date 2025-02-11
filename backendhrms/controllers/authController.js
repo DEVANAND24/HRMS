63fa45eabc2493e59ffb66fb1102f0e0da12cfd4
DEVANAND24 <raghavdas90057@gmail.com>
2025-02-11 18:41:21 +0530

    hhfk

--- backendhrms/controllers/authController.js
+++ backendhrms/controllers/authController.js
@@ -14,7 +14,9 @@ export const signup = async (req, res) => {
     // Check if the user already exists
     const existingUser = await Employee.findOne({ email });
     if (existingUser) {
+      console.error("User already exist");
       return res.status(400).json({ message: 'User already exists' });
+      
     }
     // Create a new employee record
     const user = await Employee.create({
@@ -51,6 +53,7 @@ export const login = async (req, res) => {
         token: generateToken(user._id, user.role),
       });
     } else {
+      console.error("Invalid password");
       res.status(401).json({ message: 'Invalid email or password' });
     }
   } catch (error) {
